622d697939023f567a9bca6fe7ef839d7247245a
준화 <junwha0511@naver.com>
2023-02-11 20:27:34 +0900

    Complete elantra rendering

--- background.js
+++ background.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import { OrbitControls } from "OrbitControls";
+import { OBJLoader } from "ObjectLoader";
 
 // setup //
 const renderer = new THREE.WebGLRenderer({ alpha: true });
@@ -29,16 +30,54 @@ scene.add(light);
 
 /// geometry ///
 
-const boxGeometry = new THREE.Mesh(
-    new THREE.BoxGeometry(100, 100, 100),
-    new THREE.MeshBasicMaterial({ color: 0xff0000 })
-);
-scene.add(boxGeometry);
+// const boxGeometry = new THREE.Mesh(
+//     new THREE.BoxGeometry(100, 100, 100),
+//     new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// );
+// scene.add(boxGeometry);
 
-console.log(scene);
-animate();
+const loader = new OBJLoader();
+loader.load(
+	// resource URL
+	'models/elantra.obj',
+	// called when resource is loaded
+	function ( object ) {
+        console.log(object.children[0]);
+        var meshes = [];
+        object.traverse( function (mesh) {
+            if (mesh.isMesh) {       
+                mesh.material = new THREE.MeshBasicMaterial();         
+                mesh.material.color = new THREE.Color(0x000000);
+                meshes.push(mesh);
+            } 
+        });
+        // object.children[0].color.set(0xFFB6C1);
+
+        object.scale.set(100, 100, 100);
+        scene.add( object );
+
+        function changeColor(n, defaultColor, color, t) {
+            meshes[n].material.color = new THREE.Color(color);
+            setTimeout(function(){
+                meshes[n].material.color = new THREE.Color(defaultColor);
+                changeColor((n+1)%(object.children.length-1), defaultColor, color, t);
+            }, t);
+        }
+		
+        changeColor(0, 0x000000, 0x00FFFF, 1000);
+	},
+	// called when loading is in progresses
+	function ( xhr ) {
+		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+	},
+	// called when loading has errors
+	function ( error ) {
+        console.log(error);
+	}
+);
 
 function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
 }
+animate();
